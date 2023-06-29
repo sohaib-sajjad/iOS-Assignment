f8d0dc1337e74e385441f79446f97d355b67b92e
Sohaib Sajjad <sohaib.sajjad555@gmail.com>
2023-06-30 00:47:01 +0500

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Sohaib Sajjad on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
